888cca01a40d25e55e83f8fa571fd2e989d8945f
Jonathan Whitaker <johnowhitaker@gmail.com>
2023-12-17 15:18:55 -0800

    dsc

--- mini-hw-projects/add_images.py
+++ mini-hw-projects/add_images.py
@@ -17,13 +17,13 @@ def process_markdown(file_path):
 
     image_urls = re.findall(r'!\[\]\((.*?)\)', content)
 
-    if not os.path.exists('mini-hw-projects\images'):
-        os.makedirs('mini-hw-projects\images')
+    if not os.path.exists('dsc\images'):
+        os.makedirs('dsc\images')
 
     for url in image_urls:
         if 'http' in url:
             file_name = os.path.basename(urlparse(url).path)
-            save_path = f'mini-hw-projects\images\{file_name}'
+            save_path = f'dsc\images\{file_name}'
             download_image(url, save_path)
             content = content.replace(url, f'images/{file_name}')
 
@@ -32,9 +32,11 @@ def process_markdown(file_path):
 
 # Example usage
 import glob
-
-markdown_files = ['mini-hw-projects\\2013-07-04-ir_webcam.md', 'mini-hw-projects\\2013-08-03-easy_3d_scanner.md', 'mini-hw-projects\\2013-09-02-junk_vdg.md',  'mini-hw-projects\\2013-12-01-diy_cellphone_macro_lens.md', 'mini-hw-projects\\2014-01-01-multitouch_surface.md', 'mini-hw-projects\\2023-09-30-text_to_print.md', 'mini-hw-projects\\2023-11-020-work_timer.md', 'mini-hw-projects\\bagpipes.md', 'mini-hw-projects\\cirts.md', 'mini-hw-projects\\dorm.md', 'mini-hw-projects\\kwese.md', 'mini-hw-projects\\micromouse.md', 'mini-hw-projects\\minilaser.md', 'mini-hw-projects\\mini_pcb.md', 'mini-hw-projects\\octo.md', 'mini-hw-projects\\osc.md', 'mini-hw-projects\\printer.md', 'mini-hw-projects\\seitis.md']
+markdown_files = glob.glob('dsc/*.md')
 for markdown_file in markdown_files:
     print(markdown_file)
     print("###")
-    process_markdown(markdown_file)
+    try:
+        process_markdown(markdown_file)
+    except Exception as e:
+        print(e)
